72d54337be51ce9ba96979b18d78f75fc8de0247
Arapov Vasyl <panda.dagon@gmail.com>
2022-05-24 23:27:47 +0300

    repair

---
+++ src/components/Articles/Articles.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from './Articles.module.scss'
+
+const Articles=()=>{
+    return(
+        <div>
+<h1>Articles</h1>
+        </div>
+    )
+}
+
+export default Articles;
